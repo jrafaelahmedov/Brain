b559f93b49b1a8c0fa54a3899fe42d38035b4f65
rafael <rafaelahmedovjava@gmail.com>
2018-11-28 18:03:31 +0400

    brain

---
+++ app/src/main/java/com/example/rmaahmadov/braintest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rmaahmadov.braintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.btnStart);
+
+    }
+}
